57b21bf4ca061be8cf793860f9d4ddd3f64b5647
professorisidro <isidro@professorisidro.com.br>
2020-07-29 14:44:58 -0300

    alterando url

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const URL="http://localhost:8080/validacpf?cpf=";
+const URL="https://validadordecpfdoisidro.herokuapp.com/validacpf?cpf="
 function validar(){
 
 	var txtCPF = document.getElementById("txtCpf").value;

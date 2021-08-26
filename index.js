6a5f89322471443bb8295ee0e08447f23f7d2835
professorisidro <isidro@professorisidro.com.br>
2021-08-26 09:08:43 -0300

    alterando endereço do backend

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const URL="https://validadordecpfdoisidro.herokuapp.com/validacpf?cpf="
+const URL="https://backend-validador-cpf.herokuapp.com/validacpf?cpf="
 function validar(){
 
 	var txtCPF = document.getElementById("txtCpf").value;
